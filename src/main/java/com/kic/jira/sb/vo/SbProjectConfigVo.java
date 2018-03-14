1374dad4d297e06bf942b31e3f3dd8299a047dc4
이명희 <mhlee@kicco.com>
2018-03-14 18:23:00 +0900

    SbProjectConfigVo삭제 (SbIntegrationConfigVo 로 변경)

--- src/main/java/com/kic/jira/sb/vo/SbProjectConfigVo.java
+++
@@ -1,8 +0,0 @@
-package com.kic.jira.sb.vo;
-
-import java.io.Serializable;
-
-public class SbProjectConfigVo implements Serializable {
-	private static final long serialVersionUID = -7830516092464822807L;
-
-}
