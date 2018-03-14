5891ba1e6a605eb432adb4eb46d66368d2e94bef
이명희 <mhlee@kicco.com>
2018-03-14 18:22:05 +0900

    빌드 세팅에 사용된 Vo 와 Ao 수정

--- src/main/java/com/kic/jira/sb/ao/SbInteConfig.java
+++ src/main/java/com/kic/jira/sb/ao/SbInteConfig.java
@@ -24,6 +24,23 @@ public interface SbInteConfig extends Entity {
 	String getBuildTargetName();
 	void setBuildTargetName(String dbuildTargetName);
 	
+	//buildTargetId 상태에 해당하는 stepId
+	int getBuildStepId();
+	void setBuildStepId(int deployStepId);
+	
+	
+	//빌드중 이슈 상태
+	int getBuildProgressId();
+	void setBuildProgressId(int deployProgressId);
+	
+	String getDeployProgressName();
+	void setDeployProgressName(String deployProgressName);
+	
+	//이 액션(트랜지션)을 수행하면 buildProgressId 상태가 됨.
+	int getBuildProgressAction();
+	void setBuildProgressAction(int getDeployProgressAction);
+	
+	
 	
 	//빌드성공시 수행할 트랜지션 id
 	int getBuildSuccessId();
---
+++ src/main/java/com/kic/jira/sb/vo/SbIntegerationConfigVo.java
@@ -0,0 +1,124 @@
+package com.kic.jira.sb.vo;
+
+import java.io.Serializable;
+
+import javax.xml.bind.annotation.XmlElement;
+
+public class SbIntegerationConfigVo implements Serializable {
+	private static final long serialVersionUID = -7830516092464822807L;
+
+	@XmlElement(name = "projectKey")
+	private String projectKey; //프로젝트 키
+	
+	@XmlElement(name = "issueType")
+	private String issueType; //이슈타입
+	
+	
+	@XmlElement(name = "buildTargetId")
+	private int buildTargetId; //빌드대상 상태
+	
+	@XmlElement(name = "buildTargetName")
+	private String buildTargetName; //빌드대상 상태 이름
+	
+	@XmlElement(name = "buildStepId")
+	private int buildStepId; //buildTargetId 상태에 해당하는 stepId
+	
+	
+	@XmlElement(name = "buildProgressId")
+	private String buildProgressId; //빌드중 이슈 상태
+	
+	@XmlElement(name = "buildProgressName")
+	private String buildProgressName; //빌드중 이슈 상태 이름
+	
+	@XmlElement(name = "buildProgressAction")
+	private int buildProgressAction; //이 액션(트랜지션)을 수행하면 buildProgressId 상태가 됨.
+	
+	 
+	
+	@XmlElement(name = "buildSuccessId")
+	private int buildSuccessId; //빌드 성공시 수행할 트랜지션 
+	
+	@XmlElement(name = "buildSuccessName")
+	private String buildSuccessName;//빌드 성공시 수행할 트랜지션 이름
+
+	
+	@XmlElement(name = "buildFailId")
+	private int buildFailId; //빌드 실패시 수행할 트랜지션
+	
+	@XmlElement(name = "buildFailName")
+	private String buildFailName;//빌드 실패시 수행할 트랜지션 이름
+	
+	
+	public String getProjectKey() {
+		return projectKey;
+	}
+	public void setProjectKey(String projectKey) {
+		this.projectKey = projectKey;
+	}
+	public String getIssueType() {
+		return issueType;
+	}
+	public void setIssueType(String issueType) {
+		this.issueType = issueType;
+	}
+	public int getBuildTargetId() {
+		return buildTargetId;
+	}
+	public void setBuildTargetId(int buildTargetId) {
+		this.buildTargetId = buildTargetId;
+	}
+	public String getBuildTargetName() {
+		return buildTargetName;
+	}
+	public void setBuildTargetName(String buildTargetName) {
+		this.buildTargetName = buildTargetName;
+	}
+	public int getBuildStepId() {
+		return buildStepId;
+	}
+	public void setBuildStepId(int buildStepId) {
+		this.buildStepId = buildStepId;
+	}
+	public String getBuildProgressId() {
+		return buildProgressId;
+	}
+	public void setBuildProgressId(String buildProgressId) {
+		this.buildProgressId = buildProgressId;
+	}
+	public String getBuildProgressName() {
+		return buildProgressName;
+	}
+	public void setBuildProgressName(String buildProgressName) {
+		this.buildProgressName = buildProgressName;
+	}
+	public int getBuildProgressAction() {
+		return buildProgressAction;
+	}
+	public void setBuildProgressAction(int buildProgressAction) {
+		this.buildProgressAction = buildProgressAction;
+	}
+	public int getBuildSuccessId() {
+		return buildSuccessId;
+	}
+	public void setBuildSuccessId(int buildSuccessId) {
+		this.buildSuccessId = buildSuccessId;
+	}
+	public String getBuildSuccessName() {
+		return buildSuccessName;
+	}
+	public void setBuildSuccessName(String buildSuccessName) {
+		this.buildSuccessName = buildSuccessName;
+	}
+	public int getBuildFailId() {
+		return buildFailId;
+	}
+	public void setBuildFailId(int buildFailId) {
+		this.buildFailId = buildFailId;
+	}
+	public String getBuildFailName() {
+		return buildFailName;
+	}
+	public void setBuildFailName(String buildFailName) {
+		this.buildFailName = buildFailName;
+	}
+}
